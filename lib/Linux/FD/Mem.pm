d0e05932dea911e4b256d550d636d471163102ab
Leon Timmermans <fawaka@gmail.com>
2023-01-02 01:41:06 +0100

    Improve documentation

--- lib/Linux/FD/Mem.pm
+++ lib/Linux/FD/Mem.pm
@@ -13,13 +13,19 @@ XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
 =head1 SYNOPSIS
 
- use Linux::FD::Mem
+ use Linux::FD::Mem;
  
- my $fh = Linux::FD::Mem->new();
+ my $fh = Linux::FD::Mem->new('name', 'allow-seals');
+ ... # write-map the file
+ $fh->seal('future-write', 'shrink', 'grow', 'write');
+
+=head1 DESCRIPTION
+
+Linux::FD::Mem provides memory file descriptors; these will not be represented on the file-system in any way. They can be made to use seals or larger page-sizes.
 
 =method new($name, @flags)
 
-This creates an anonymous file and returns a file descriptor that refers to it. The file behaves like a regular file, and so can be modified, truncated, memory-mapped, and so on. However, unlike a regular file, it lives in RAM and has a volatile backing storage. Once all references to the file are dropped, it is automatically released. Anonymous memory is used for all backing pages of the file. Therefore, files created by this module have the same semantics as other anonymous memory allocations such as those allocated using L<File::Map|File::Map>'s C<map_anonymous>.
+This creates an anonymous file and returns a file handle that refers to it. The file behaves like a regular file, and so can be modified, truncated, memory-mapped, and so on. However, unlike a regular file, it lives in RAM and has a volatile backing storage. Once all references to the file are dropped, it is automatically released. Anonymous memory is used for all backing pages of the file. Therefore, files created by this module have the same semantics as other anonymous memory allocations such as those allocated using L<File::Map|File::Map>'s C<map_anonymous>.
 
 The name supplied in C<$name> is used as a filename and will be displayed as the target of the corresponding symbolic link in the directory /proc/self/fd/. The displayed name is always prefixed with memfd: and serves only for debugging purposes. Names do not affect the behavior of the file descriptor, and as such multiple files can have the same name without any side effects.
 
